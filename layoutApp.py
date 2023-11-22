353cb078966cf8dc8bc597644796acf017e71be8
elmooooooooooo <mohcine.elouahabi@gmail.com>
2023-11-22 09:00:40 +0100

    rekenmachine layout

---
+++ layoutApp.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+app = tk.Tk()
+
+knop1 = tk.Button(master=app, text="1", width=5, height=2)
+knop1.grid(row=1, column=0)
+
+knop1 = tk.Button(master=app, text="1", width=5, height=2)
+knop1.grid(row=1, column=1)
+
+knop1 = tk.Button(master=app, text="1", width=5, height=2)
+knop1.grid(row=1, column=2)
+
+knop1 = tk.Button(master=app, text="1", width=5, height=2)
+knop1.grid(row=2, column=0)
+
+knop1 = tk.Button(master=app, text="1", width=5, height=2)
+knop1.grid(row=2, column=1)
+
+knop1 = tk.Button(master=app, text="1", width=5, height=2)
+knop1.grid(row=2, column=2)
+
+app.mainloop()
