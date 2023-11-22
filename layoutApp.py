380ebfde39ea92edc4678155d25bd5691f7e4f32
elmooooooooooo <mohcine.elouahabi@gmail.com>
2023-11-22 10:02:11 +0100

    YoutubeLogo

--- layoutApp.py
+++ layoutApp.py
@@ -1,10 +1,15 @@
 import tkinter as tk
 app = tk.Tk()
+from PIL import Image, ImageTk
+
+logo = Image.open(r"afbeeldingen/Youtube_logo.png")
+logo = logo.resize((64,64))
+logo = ImageTk.PhotoImage(logo)
 
 titel = tk.Label(master=app, text= "Choose your platform: ")
 titel.grid(row=0, columnspan=3)
 
-knop1 = tk.Button(master=app, text="1", width=25, height=5)
+knop1 = tk.Button(master=app, text="youtube",image=logo, compound=tk.BOTTOM, width=180)
 knop1.grid(row=1, column=0)
 
 knop1 = tk.Button(master=app, text="1", width=25, height=5)
