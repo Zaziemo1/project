32a34380680e7734a42586276bf2611b354801ad
elmooooooooooo <mohcine.elouahabi@gmail.com>
2023-11-29 09:01:55 +0100

    uhhçu çu  zhe

--- app.py
+++ app.py
@@ -2,14 +2,61 @@ import tkinter as tk
 from tkinter import messagebox
 from tkinter import ttk
 from pytube import YouTube
+import os
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from pydub import AudioSegment
+import requests
+from io import BytesIO
 
-def download_audio():
-    url = url_entry2.get()
-    messagebox.showinfo('Download Complete', f'Download complete {url.title}')
+client_id = "4cc048fb419440f5951860cf0a9f9db0"
+client_secret = "763a491995ce4b1cad73ecccd6bb9ade"
+client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+def download_Spotifysong(track_uri):
+    try:
+        track_info = sp.track(track_uri)
+        track_name = track_info['name']
+        artist_name = track_info['artists'][0]['name']
+        
+        # Get the preview URL
+        preview_url = track_info.get('preview_url')
+        if not preview_url:
+            raise ValueError("Preview URL not available for the selected track.")
+        
+        # Download the audio data
+        audio_data = download_Spotifyaudio(preview_url)
+        
+        # Convert to AudioSegment
+        audio_segment = AudioSegment.from_file(BytesIO(audio_data), format="mp3")
+        
+        # Saving the audio segment to an MP3 file
+        download_path = os.path.expanduser('~') + '/Downloads/'
+        mp3_filename = f"{artist_name} - {track_name}.mp3"
+        mp3_path = os.path.join(download_path, mp3_filename)
+        audio_segment.export(mp3_path, format="mp3")
+
+        messagebox.showinfo("Download Complete", "Song downloaded and converted to MP3 successfully.")
+    except Exception as e:
+        print(f"Error downloading song: {e}")
+        messagebox.showerror("Error", "Unable to download song.")
+
+def download_Spotifyaudio(url):
+    try:
+        response = requests.get(url)
+        return response.content
+    except Exception as e:
+        print(f"Error downloading audio: {e}")
+        return None
+
+
+
+
         
 
 
-def download_video():
+def download_Youtubevideo():
     url = url_entry.get()
 
     try:
@@ -50,11 +97,11 @@ tabControl.pack(expand=1, fill="both")
 
 
 # Download yt
-download_button = tk.Button(tab1, text='Download Video', command=download_video)
+download_button = tk.Button(tab1, text='Download Video', command=download_Youtubevideo)
 download_button.pack(pady=20)
 
 # Download Spotify
-download_button2 = tk.Button(tab2, text='Download Audio',command=download_audio)
+download_button2 = tk.Button(tab2, text='Download Audio',command= lambda : download_Spotifysong(url_entry2.get()))
 download_button2.pack(pady=20)
 
 # Run the Tkinter event loop
